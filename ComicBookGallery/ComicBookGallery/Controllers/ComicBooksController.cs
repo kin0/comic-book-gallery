60a28501f1f635b79f13752f5b575830790dd37b
kino <kevin@kevinlopez.net>
2019-04-28 11:37:40 -0400

    controller setup done

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -12,12 +12,15 @@ namespace ComicBookGallery.Controllers
         {
             if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
             {
-                return new RedirectResult("/");
+                return Redirect("/");
+                //return new RedirectResult("/");
             }
-            return new ContentResult()
+            return Content("Hello from the comic book page!");
+           /* return new ContentResult()
             {
                 Content = "Hello from the comic book page!"
-            };
+            };*/
+            // Note this is all in green because thanks to the Controller MVC controller base class since 'we inheritid its functions
         }
     }
 }
