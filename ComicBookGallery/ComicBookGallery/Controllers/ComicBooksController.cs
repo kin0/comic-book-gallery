022d202946b2d8b7775631a4b5a6c80c6d733775
kino <kevin@kevinlopez.net>
2019-04-28 11:57:55 -0400

    Added Comic Book Deatil View

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,17 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-                //return new RedirectResult("/");
-            }
-            return Content("Hello from the comic book page!");
-           /* return new ContentResult()
-            {
-                Content = "Hello from the comic book page!"
-            };*/
-            // Note this is all in green because thanks to the Controller MVC controller base class since 'we inheritid its functions
+            return View();
         }
     }
 }
